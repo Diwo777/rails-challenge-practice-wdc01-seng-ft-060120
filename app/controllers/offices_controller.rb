9610b752b7480cca847e2286627e058814263cad
estherkang14 <estherkang14@gmail.com>
2020-06-30 11:13:30 -0400

    finished setting up and complete building reqs

---
+++ app/controllers/offices_controller.rb
@@ -0,0 +1,13 @@
+class OfficesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- app/models/building.rb
+++ app/models/building.rb
@@ -1,4 +1,6 @@
 class Building < ApplicationRecord
+  has_many :offices
+  has_many :companies, through: :offices
 
   def number_of_floors_available
     # Will not work until relationships and schema are corretly setup
@@ -13,5 +15,19 @@ class Building < ApplicationRecord
   def empty_offices
     number_of_floors_available.map { |f| offices.build(floor: f) }
   end
+
+  def unique_companies 
+    self.companies.uniq
+  end 
+
+  def total_rent
+    # rent_per_floor 18000
+    # number_of_floors - number_of_floors_available.count = number_of_floors_taken 
+    # 8 - 1 = 7
+    # rent_per_floor * number_of_floors_taken = total_rent 
+    number_of_floors_taken = self.number_of_floors - self.number_of_floors_available.count 
+    total = rent_per_floor * number_of_floors_taken
+    total 
+  end 
 
 end
