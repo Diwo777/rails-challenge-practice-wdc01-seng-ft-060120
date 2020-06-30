d6ea17d0d334c93655fc23893998721c040640c8
Diwo777 <diego.zegarra79@icloud.com>
2020-06-30 12:10:58 -0400

    updated company show page

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,13 @@ class Company < ApplicationRecord
     has_many :buildings, through: :offices
     has_many :employees
     
+  def unique_buildings
+    self.buildings.uniq
+  end
+
+  def office_floors
+    self.offices
+  end
+
 end
+ 
